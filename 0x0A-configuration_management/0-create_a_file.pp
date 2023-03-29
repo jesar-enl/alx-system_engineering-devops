4bdd389ac155897036c1e71d0802688586e9e012
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-03-29 12:06:19 +0300

    Puupet file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Contains puppet DSL specification which creates a new file
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
